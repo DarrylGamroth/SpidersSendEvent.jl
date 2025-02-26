0a377e3d8bb942a0d930d7d920839d95a0abdf48
Darryl Gamroth <dgamroth@rubus.ca>
2025-02-26 14:37:36 -0800

    Script updates

--- SpidersSendEvent.ps1
+++ SpidersSendEvent.ps1
@@ -5,10 +5,8 @@ $env:STATUS_STREAM_ID = 1
 $env:CONTROL_URI = "aeron:udp?endpoint=0.0.0.0:40123"
 $env:CONTROL_STREAM_ID = 2
 $env:CONTROL_STREAM_FILTER = "Camera"
-$env:PUB_DATA_URI_1 = "aeron:udp?endpoint=132.246.192.209:40123"
-$env:PUB_DATA_STREAM_1 = 3
 $env:BLOCK_NAME = "Camera"
-$env:BLOCK_ID = 367
+$env:BLOCK_ID = 1023
 
 # Run the Julia script
 & "julia" -e "using SpidersSendEvent; SpidersSendEvent.main(ARGS)" -- $args
--- SpidersSendEvent.sh
+++ SpidersSendEvent.sh
@@ -7,8 +7,6 @@ export STATUS_STREAM_ID=1
 export CONTROL_URI="aeron:udp?endpoint=0.0.0.0:40123"
 export CONTROL_STREAM_ID=2
 export CONTROL_STREAM_FILTER="Camera"
-export PUB_DATA_URI_1="aeron:udp?endpoint=132.246.192.209:40123"
-export PUB_DATA_STREAM_1=3
 export BLOCK_ID=1023
 
 # Run the Julia script
